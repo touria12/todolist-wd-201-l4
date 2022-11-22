3584b398ff7d3fc5c84d64667165a4852ab199be
touria tanazzum <ttanazzum@gmail.com>
2022-11-22 19:06:28 +0530

    Add lint-staged

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -4,13 +4,13 @@ const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 describe("Checking my todolist", () => {
   test("checks creating a new todo", () => {
     //expect(all.length).toBe(0);
-    let l = all.length;
+    let arr_len = all.length;
     add({
       title: "Clening",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
-    expect(all.length).toBe(l + 1);
+    expect(all.length).toBe(arr_len + 1);
   });
   test("checks Marking a todo as completed", () => {
     expect(all[0].completed).toBe(false);
@@ -18,26 +18,26 @@ describe("Checking my todolist", () => {
     expect(all[0].completed).toBe(true);
   });
   test("checks retreival of all todos that are overdue", () => {
-    let todosList = overdue();
+    let t_lists = overdue();
     expect(
-      todosList.every((todo) => {
-        return todo.dueDate < new Date().toLocaleDateString("en-CA");
+      t_lists.every((t) => {
+        return t.dueDate < new Date().toLocaleDateString("en-CA");
       })
     ).toBe(true);
   });
   test("checks retrieval of all todos that are dueToday", () => {
-    let todosList = dueToday();
+    let t_lists = dueToday();
     expect(
-      todosList.every((todo) => {
-        return todo.dueDate === new Date().toLocaleDateString("en-CA");
+      t_lists.every((t) => {
+        return t.dueDate === new Date().toLocaleDateString("en-CA");
       })
     ).toBe(true);
   });
   test("checks retrival of all todos that are dueLater", () => {
-    let todosList = dueLater();
+    let t_lists = dueLater();
     expect(
-      todosList.every((todo) => {
-        return todo.dueDate > new Date().toLocaleDateString("en-CA");
+      t_lists.every((t) => {
+        return t.dueDate > new Date().toLocaleDateString("en-CA");
       })
     ).toBe(true);
   });
