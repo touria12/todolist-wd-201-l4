96d425c02910bed738f08f3bd43365e79ce7865e
touria tanazzum <ttanazzum@gmail.com>
2022-11-22 18:42:32 +0530

    Add pre-commit hook to run tests

---
+++ __test__/todo.js
@@ -0,0 +1,43 @@
+const todoList = require("../todo");
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+describe("Checking my todolist", () => {
+  test("checks creating a new todo", () => {
+    //expect(all.length).toBe(0);
+    let l = all.length;
+    add({
+      title: "Clening",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(l + 1);
+  });
+  test("checks Marking a todo as completed", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("checks retreival of all todos that are overdue", () => {
+    let todosList = overdue();
+    expect(
+      todosList.every((todo) => {
+        return todo.dueDate < new Date().toLocaleDateString("en-CA");
+      })
+    ).toBe(true);
+  });
+  test("checks retrieval of all todos that are dueToday", () => {
+    let todosList = dueToday();
+    expect(
+      todosList.every((todo) => {
+        return todo.dueDate === new Date().toLocaleDateString("en-CA");
+      })
+    ).toBe(true);
+  });
+  test("checks retrival of all todos that are dueLater", () => {
+    let todosList = dueLater();
+    expect(
+      todosList.every((todo) => {
+        return todo.dueDate > new Date().toLocaleDateString("en-CA");
+      })
+    ).toBe(true);
+  });
+});
---
+++ todo.js
@@ -0,0 +1,55 @@
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  let today = new Date().toISOString().split("T")[0];
+
+  const overdue = () => {
+    return all.filter((todo) => {
+      return todo.dueDate < today;
+    });
+  };
+
+  const dueToday = () => {
+    return all.filter((todo) => {
+      return todo.dueDate === today;
+    });
+  };
+
+  const dueLater = () => {
+    return all.filter((todo) => {
+      return todo.dueDate > today;
+    });
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map((todo) => {
+        bahvya = todo.completed ? "[x]" : "[ ]";
+        tauriya = todo.dueDate == today ? "" : todo.dueDate;
+
+        return `${bahvya} ${todo.title} ${tauriya}`;
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
